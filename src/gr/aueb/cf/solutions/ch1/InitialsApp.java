8e87e8f6c89e28c830a245bda195ac74da951494
a8ana <a8anassis@gmail.com>
2023-10-29 06:50:47 +0200

    solutions ch1

---
+++ src/gr/aueb/cf/solutions/ch1/InitialsApp.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.solutions.ch1;
+
+/**
+ * Εκτυπώνει τα αρχικά του ονόματος
+ * με αστεράκια.
+ */
+public class InitialsApp {
+
+    public static void main(String[] args) {
+        System.out.println("*********           * ");
+        System.out.println("*       *          * *");
+        System.out.println("*       *         *   *");
+        System.out.println("*********        *******");
+        System.out.println("*       *       *       *");
+        System.out.println("*       *      *         *");
+        System.out.println("*********     *           *");
+    }
+}
---
+++ src/gr/aueb/cf/solutions/ch1/MenuApp.java
@@ -0,0 +1,17 @@
+package gr.aueb.cf.solutions.ch1;
+
+/**
+ * Εκτυπώνει ένα μενού.
+ */
+public class MenuApp {
+
+    public static void main(String[] args) {
+        System.out.println("Επιλέξτε μία από τις παρακάτω επιλογές");
+        System.out.println("1. Εισαγωγή");
+        System.out.println("2. Διαγραφή");
+        System.out.println("3. Αναζήτηση");
+        System.out.println("4. Ενημέρωση");
+        System.out.println("5. Έξοδος");
+        System.out.println("Δώστε αριθμό επιλογής:");
+    }
+}
