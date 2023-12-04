8475893a4ba97e2ace179c03585e9a37bc3ae4f0
a8ana <a8anassis@gmail.com>
2023-12-04 20:55:47 +0200

    IO Read 2

---
+++ src/gr/aueb/cf/ch9/IOCopy2.java
@@ -0,0 +1,33 @@
+package gr.aueb.cf.ch9;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class IOCopy2 {
+
+    public static void main(String[] args) {
+        int b;
+        int count = 0;
+        long start;
+        long end;
+        double elapsedTime;
+        byte[] buf = new byte[8192];
+
+        try (FileInputStream in = new FileInputStream("C:/tmp/v1.mp4");
+             FileOutputStream out = new FileOutputStream("C:/tmp/new-v1.mp4")) {
+
+            start = System.nanoTime();
+            while ((b = in.read(buf)) != -1) {
+                out.write(buf, 0, b);
+                count += b;
+            }
+            end = System.nanoTime();
+            elapsedTime = (end - start) / 1_000_000_000.0;
+            System.out.printf("To αρχείο με μέγεθος %d KBytes (%d bytes) αντιγράφηκε\n", count / 1024, count);
+            System.out.printf("Elapsed Time: %.2f seconds", elapsedTime);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
