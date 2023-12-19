88c7b1471232decdc57160f093bc478da91d4ccf
a8ana <a8anassis@gmail.com>
2023-12-19 20:07:07 +0200

    Account class with Public API

--- src/gr/aueb/cf/ch11/Account.java
+++ src/gr/aueb/cf/ch11/Account.java
@@ -24,47 +24,36 @@ public class Account {
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getIban() {
         return iban;
     }
-
     public void setIban(String iban) {
         this.iban = iban;
     }
-
     public String getFirstname() {
         return firstname;
     }
-
     public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
-
     public String getLastname() {
         return lastname;
     }
-
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
-
     public String getSsn() {
         return ssn;
     }
-
     public void setSsn(String ssn) {
         this.ssn = ssn;
     }
-
     public double getBalance() {
         return balance;
     }
-
     public void setBalance(double balance) {
         this.balance = balance;
     }
@@ -94,16 +83,37 @@ public class Account {
         }
     }
 
-    public void withdraw(double amount) throws Exception {
+    /**
+     * Withdraws a certain amount of money based on a
+     * valid ssn.
+     *
+     * @param amount
+     *      the amount of money to be withdrawn.
+     * @param ssn
+     *      the given ssn.
+     * @throws Exception
+     *      if the ssn is not valid or if the balance
+     *      is not sufficient.
+     */
+    public void withdraw(double amount, String ssn) throws Exception {
         try {
             if (amount > balance) {
                 throw new Exception("Insufficient balance error");
             }
+
+            if (!isSsnValid(ssn)) {
+                throw new Exception("Bad ssn");
+            }
+
             balance -= amount;
         } catch (Exception e) {
             e.printStackTrace();
             throw e;
         }
+    }
+
+    private boolean isSsnValid(String ssn) {
+        return this.ssn.equals(ssn);
     }
 
     /**
