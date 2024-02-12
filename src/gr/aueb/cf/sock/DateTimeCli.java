272efffb16b9805bb97dc60a4b7c7c75bf5168a5
a8ana <a8anassis@gmail.com>
2024-02-12 19:46:21 +0200

    DateTimeCli

---
+++ src/gr/aueb/cf/sock/DateTimeCli.java
@@ -0,0 +1,37 @@
+package gr.aueb.cf.sock;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class DateTimeCli {
+
+    public static void main(String[] args) {
+        Socket socket = null;
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            InetAddress servAddress = InetAddress.getByName("time.nist.gov");
+            int servPort = 13;
+
+            socket = new Socket(servAddress, servPort);
+
+            BufferedReader bf = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String line = "";
+            while ((line = bf.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            System.out.println("DateTime Server says: " + sb);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (socket != null) socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
